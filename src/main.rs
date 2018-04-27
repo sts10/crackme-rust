01f003f670c5fa915ea1cfacb97baa8a2dca32d9
sts10 <sschlinkert@gmail.com>
2018-04-26 21:31:49 -0400

    still getting incorrect derived key

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
 extern crate ring;
 
 use ring::{digest, pbkdf2};
+// https://briansmith.org/rustdoc/ring/pbkdf2/index.html
 use std::fmt::Write;
-// use std::collections::HashMap;
 
 static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA256;
-const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
+const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN; // or just put 32
 pub type Credential = [u8; CREDENTIAL_LEN];
 
 fn main() {
-    let password = "node tuesday imperial";
-    let salt = "e628cf2534f66ca172b1ebd82394563a".as_bytes();
+    let password = "tanbark artistic callus";
+    let salt = "00bb202b205f064e30f6fae101162a2e";
+    // might able to just do 'let salt = "e628cf2534f66ca172b1ebd82394563a".as_bytes();`
+    // but I'm not sure
+    let mut salt_vec = Vec::new(); // gonna be Vec<u8>
+    for c in salt.chars() {
+        salt_vec.push(c as u8);
+    }
+
+    println!("salt vexc is {:?}", salt_vec);
+
     let iterations = 100000;
     let mut to_store: Credential = [0u8; CREDENTIAL_LEN];
     pbkdf2::derive(
         DIGEST_ALG,
         iterations,
-        &salt,
+        &salt_vec,
         password.as_bytes(),
         &mut to_store,
     );
 
     println!("out: {:?}", to_store);
 
-    let mut upper = String::new();
     let mut lower = String::new();
     for &byte in to_store.iter() {
-        write!(&mut upper, "{:X}", byte);
         write!(&mut lower, "{:x}", byte);
     }
 
     println!("as string {}", lower);
+    // I get e6941d24abc7fb69af91d69b8710175c3f7ac67969ec1fa8f7cf819c8d3e7
+    println!("should be 91976be95cd28e55e580ee9f69a2139202a9b65eabfbbf33c99bc42e3665564d");
 }

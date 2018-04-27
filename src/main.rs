a15aca2e55bc92dc0798fb626b41b7756b514fab
sts10 <sschlinkert@gmail.com>
2018-04-26 20:58:39 -0400

    lowercased write

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
 pub type Credential = [u8; CREDENTIAL_LEN];
 
 fn main() {
-    let password = "tanbark artistic callus";
-    let salt = "00bb202b205f064e30f6fae101162a2e".as_bytes();
+    let password = "node tuesday imperial";
+    let salt = "e628cf2534f66ca172b1ebd82394563a".as_bytes();
     let iterations = 100000;
     let mut to_store: Credential = [0u8; CREDENTIAL_LEN];
     pbkdf2::derive(
@@ -21,12 +21,14 @@ fn main() {
         &mut to_store,
     );
 
-    // println!("out: {:?}", to_store);
+    println!("out: {:?}", to_store);
 
-    let mut s = String::new();
+    let mut upper = String::new();
+    let mut lower = String::new();
     for &byte in to_store.iter() {
-        write!(&mut s, "{:X}", byte);
+        write!(&mut upper, "{:X}", byte);
+        write!(&mut lower, "{:x}", byte);
     }
 
-    println!("as string {}", s);
+    println!("as string {}", lower);
 }

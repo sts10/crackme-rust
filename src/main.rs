7ce43a2625f592c803bd7dac57d6a5b01c1bcda5
sts10 <sschlinkert@gmail.com>
2018-04-27 21:31:55 -0400

    work slowly

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,90 @@ extern crate ring;
 use ring::{digest, pbkdf2};
 // https://briansmith.org/rustdoc/ring/pbkdf2/index.html
 use std::fmt::Write;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
 
 static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA256;
 const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN; // or just put 32
 pub type Credential = [u8; CREDENTIAL_LEN];
 
 fn main() {
-    let password = "tanbark artistic callus";
     let salt = "00bb202b205f064e30f6fae101162a2e";
+    let derived = "dc61e18eaa1add3e555cd493acb9088449d2ac07a739eec15cd299a327bf45b0";
     // let password = "node tuesday imperial";
     // let salt = "e628cf2534f66ca172b1ebd82394563a";
     let iterations = 100000;
+    run_crack2(iterations, salt, derived);
+}
+// fn run_crack(given_iterations: u32, given_salt: &str, given_derived: &str) -> String {
+//     let first_words = make_word_list("agile_words.txt");
+//     let second_words = make_word_list("agile_words.txt");
+//     let third_words = make_word_list("agile_words.txt");
+//     for first_word in first_words {
+//         for second_word in second_words {
+//             for third_word in third_words {
+//                 let password_guess = first_word + " " + &second_word + " " + &third_word;
+//                 if guess(&password_guess, given_iterations, given_salt, given_derived) {
+//                     println!("Found it! {}", password_guess);
+//                     return password_guess;
+//                 }
+//             }
+//         }
+//     }
+//     return "Didn't crack".to_string();
+// }
 
-    println!("I got {}", derive(iterations, salt, password));
-    println!("shd b 91976be95cd28e55e580ee9f69a2139202a9b65eabfbbf33c99bc42e3665564d");
+fn run_crack2(given_iterations: u32, given_salt: &str, given_derived: &str) {
+    let words1 = make_word_list("agile_words.txt");
+    let words2 = make_word_list("agile_words.txt");
+    let words3 = make_word_list("agile_words.txt");
+    let mut password_guess = String::new();
+    let mut password_guess_vec: Vec<&str> = [].to_vec();
+
+    for i in 0..words1.len() {
+        let first_word = &words1[i];
+        password_guess_vec.push(first_word);
+        for j in 0..words2.len() {
+            let second_word = &words2[j];
+            // password_guess_vec.push(" ");
+            password_guess_vec.push(second_word);
+            for k in 0..words3.len() {
+                // password_guess_vec.push(" ");
+                password_guess_vec.push(&words3[k]);
+                password_guess = password_guess_vec.join(" ");
+                // password_guess = first_word + " " + &second_word + " " + &words3[k];
+                if guess(&password_guess, given_iterations, given_salt, given_derived) {
+                    println!("Found it! {}", password_guess);
+                // return password_guess;
+                } else {
+                    println!("Tried {} unsuccessfully", password_guess);
+                    // clear third_word
+                    password_guess_vec.truncate(2);
+                    println!("vec is now {:?}", password_guess_vec);
+                }
+            }
+            // clear second_word
+            password_guess_vec.truncate(1);
+            println!("vec is now {:?}", password_guess_vec);
+        }
+        // clear first_word
+        password_guess_vec = [].to_vec();
+    }
+    // let fail = "didn't crack";
+    // return fail;
+}
+
+fn make_word_list(filename: &str) -> (Vec<String>) {
+    let mut words_vec: Vec<String> = [].to_vec();
+
+    let f = File::open(filename).unwrap();
+    let file = BufReader::new(&f);
+    for line in file.lines() {
+        let l = line.unwrap();
+        words_vec.push(String::from(l));
+    }
+    return words_vec;
 }
 fn guess(password_guess: &str, iterations: u32, salt: &str, derived: &str) -> bool {
     if derive(iterations, salt, password_guess) == derived {

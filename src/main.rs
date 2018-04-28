7e2f3bce38fc221d893f8ff79b25feefc9c9cb65
sts10 <sschlinkert@gmail.com>
2018-04-27 20:27:19 -0400

    implements three tests for the derive function

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,10 @@ fn main() {
     // let salt = "e628cf2534f66ca172b1ebd82394563a";
     let iterations = 100000;
 
-    derive(iterations, salt, password);
+    println!("I got {}", derive(iterations, salt, password));
+    println!("shd b 91976be95cd28e55e580ee9f69a2139202a9b65eabfbbf33c99bc42e3665564d");
 }
-fn derive(iterations: u32, salt: &str, password: &str) {
+fn derive(iterations: u32, salt: &str, password: &str) -> String {
     // first, make salt_vec (thanks to https://stackoverflow.com/a/44532957)
     let mut salt_vec = Vec::new();
     for i in 0..(salt.len() / 2) {
@@ -43,11 +44,29 @@ fn derive(iterations: u32, salt: &str, password: &str) {
     for &byte in derived_hash.iter() {
         write!(&mut lower, "{:02x}", byte);
     }
+    return lower;
+}
 
-    println!("as string {}", lower);
-    println!("should be 91976be95cd28e55e580ee9f69a2139202a9b65eabfbbf33c99bc42e3665564d");
-    println!("should be 703daeb5f1d90feaee8a62273eedbe539e1c7db158f3d720ab9264e3e8e08e6a");
-    // according to https://github.com/agilebits/crackme/blob/master/doc/answers-2018-03-30.json#L9
-    //
-    // return lower.as_str();
+#[test]
+fn derive_example1() {
+    let password = "tanbark artistic callus";
+    let salt = "00bb202b205f064e30f6fae101162a2e";
+    let derived = "91976be95cd28e55e580ee9f69a2139202a9b65eabfbbf33c99bc42e3665564d";
+    assert_eq!(derive(100000, salt, password), derived);
+}
+
+#[test]
+fn derive_example2() {
+    let password = "node tuesday imperial";
+    let salt = "e628cf2534f66ca172b1ebd82394563a";
+    let derived = "703daeb5f1d90feaee8a62273eedbe539e1c7db158f3d720ab9264e3e8e08e6a";
+    assert_eq!(derive(100000, salt, password), derived);
+}
+
+#[test]
+fn derive_example3() {
+    let password = "ambulant horsefly capstone";
+    let salt = "5ed20a9710cddc0278cbb345c5a4ac3d";
+    let derived = "524098d90da74ed38cb86a5ebf5f4cecd39e52d200d7c32b4606731d5696b3d5";
+    assert_eq!(derive(100000, salt, password), derived);
 }
